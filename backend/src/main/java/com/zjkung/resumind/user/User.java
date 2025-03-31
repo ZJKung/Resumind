2971d6aa66f82fdf4b336a4cfc4e59626df61eb5
zjkung1123 <zjkung1123@gmail.com>
2025-03-30 20:56:05 -0400

    Refactor User entity to include password hash and full name fields; update database URL in application properties for correct schema usage.

--- backend/src/main/java/com/zjkung/resumind/user/User.java
+++ backend/src/main/java/com/zjkung/resumind/user/User.java
@@ -1,15 +1,20 @@
 package com.zjkung.resumind.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "\"user\"")
 public class User {
   @Id
   @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
   private Long id;
   private String email;
+  @Column(name = "password_hash")
   private String passwordhash;
+  @Column(name = "full_name")
   private String fullname;
 }
